757259c2486d43e6b4263c4a3071e42992f05bb6
Sophie Chen <sophie.chen@featurespace.co.uk>
2024-01-20 15:55:52 +0000

    Initial MealPlanner outline

--- src/main/kotlin/io/hsar/mealplanner/MealPlanner.kt
+++ src/main/kotlin/io/hsar/mealplanner/MealPlanner.kt
@@ -1,10 +1,10 @@
 package io.hsar.mealplanner
 
-import io.hsar.mealplanner.storage.JsonStorage
 import io.hsar.mealplanner.storage.MealOptionStorage
 import io.hsar.mealplanner.storage.PreviousMealStorage
 import java.time.LocalDate
-import java.util.Date
+import java.util.*
+import kotlin.random.Random
 
 /**
  * Main class responsible for the overall generation of meal plans.
@@ -12,6 +12,19 @@ import java.util.Date
 class MealPlanner(val storage: PreviousMealStorage, val mealOptions: MealOptionStorage) {
     fun generate(days: Int, fromDate: LocalDate = LocalDate.now()) {
         val possibleMeals = mealOptions.read()
-        TODO("Not yet implemented")
+        val seed = Objects.hashCode(fromDate)
+
+        val firstIndex = Random(seed).nextInt()
+
+        /*
+        find starting place
+        iterate through checking how many days we've got and how many portions a meal provides
+        prioritise higher portion numbers
+
+        Things to consider when selecting options:
+        - when the meal was last used
+        - whether the tags are appropriate
+         */
+
     }
 }
